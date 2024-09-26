9a46d441afa0080a4102d04789e78ad5ac47b873
Александра Черчел <aleks.tcherchel@yandex.ru>
2024-09-26 19:59:14 +0300

    правк по замечанию

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,12 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
+    if number == 0:
+        return 1
     if len(str_number) == 1:
         return first
     return first * get_multiplied_digits(int(str_number[1:]))
 
 
-print(get_multiplied_digits(40203))
+print(get_multiplied_digits(420))
 

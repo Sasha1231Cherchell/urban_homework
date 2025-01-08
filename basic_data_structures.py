8bab5e03e4662c8ed37c02947e81d38cf9364309
Александра Черчел <aleks.tcherchel@yandex.ru>
2025-01-08 16:03:50 +0300

    basic data structures

---
+++ basic_data_structures.py
@@ -0,0 +1,17 @@
+
+
+# 1st program
+print(9**0.5 * 5)
+
+# 2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# 4th program
+s = '123.456'
+n = float(s)
+print(int(n * 10) % 10)
